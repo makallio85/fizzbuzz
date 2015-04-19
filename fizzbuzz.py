7db43337ccf5b8f51e10a5afe3d8852428cd5f5a
Marko Kallio <makallio85@users.noreply.github.com>
2015-04-19 09:35:32 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,18 @@
-# See about Fizz buzz in here, http://en.wikipedia.org/wiki/Fizz_buzz
+# See about Fizz buzz game in here, http://en.wikipedia.org/wiki/Fizz_buzz
 
-def doRun(toNumber):
-    for x in range(1, toNumber + 1):
+import time
+
+#Whether to print each row result in iteration on not
+printRounds = False
+
+#Get time in milliseconds
+def currMillTime():
+    return round(time.time() * 1000)
+
+#Run rounds
+def doRun(amount, printRounds):
+    start = currMillTime()
+    for x in range(1, amount + 1):
         string = ""
         if x % 3 == 0:
             string = "Fizz"
@@ -9,5 +20,18 @@ def doRun(toNumber):
             string = string + "Buzz"
         if string == "":
             string = x
-        print(string)
-doRun(100)
+        if printRounds:
+            print(string)
+    end = currMillTime()
+    print(getResultString(start, end, amount))
+
+#Get result string to print after rounds are iterated
+def getResultString(start, end, amount):
+    duration = end - start
+    return "It took " + str(duration) + " milliseconds to run " + str(amount) + " rounds in Fizz buzz game."
+
+#Run game with different amounts of rounds
+amount = 10
+while (amount <= 10000000):
+    doRun(amount, printRounds)
+    amount = amount * 10
