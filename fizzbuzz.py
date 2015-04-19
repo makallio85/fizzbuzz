519f600ee0cdfa73954c103cbb0be50229b168b6
Marko Kallio <makallio85@users.noreply.github.com>
2015-04-19 10:14:01 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,16 +2,19 @@
 
 import time
 
-#Whether to print each row result in iteration on not
+# Whether to print each row result in iteration on not
 printRounds = False
 
-#Get time in milliseconds
-def currMillTime():
+# Starting rounds amount for game
+amount = 10
+
+# Get time in milliseconds
+def curr_mill_time():
     return round(time.time() * 1000)
 
-#Run rounds
-def doRun(amount, printRounds):
-    start = currMillTime()
+# Run game
+def do_run(amount, printRounds):
+    start = curr_mill_time()
     for x in range(1, amount + 1):
         string = ""
         if x % 3 == 0:
@@ -22,16 +25,15 @@ def doRun(amount, printRounds):
             string = x
         if printRounds:
             print(string)
-    end = currMillTime()
-    print(getResultString(start, end, amount))
+    end = curr_mill_time()
+    print(get_result_string(start, end, amount))
 
-#Get result string to print after rounds are iterated
-def getResultString(start, end, amount):
+# Get result string to print after rounds are iterated
+def get_result_string(start, end, amount):
     duration = end - start
-    return "It took " + str(duration) + " milliseconds to run " + str(amount) + " rounds in Fizz buzz game."
+    return "It took {} milliseconds to run {} rounds in Fizz buzz game.".format(duration, amount)
 
-#Run game with different amounts of rounds
-amount = 10
+# Run game with different round amounts
 while (amount <= 10000000):
-    doRun(amount, printRounds)
+    do_run(amount, printRounds)
     amount = amount * 10
